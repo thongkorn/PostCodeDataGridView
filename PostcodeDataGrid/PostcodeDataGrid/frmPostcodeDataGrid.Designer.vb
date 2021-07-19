b5cb8928c829243efb91e4fc1bbf04b8219b2b34
Thongkorn Tubtimkrob <thongkorn@hotmail.com>
2021-07-19 15:59:12 +0700

    Add files via upload

---
+++ PostcodeDataGrid/PostcodeDataGrid/frmPostcodeDataGrid.Designer.vb
@@ -0,0 +1,86 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmPostcodeDataGrid
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.btnAddRow = New System.Windows.Forms.Button()
+        Me.btnRemoveRow = New System.Windows.Forms.Button()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(0, 0)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(767, 373)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'btnAddRow
+        '
+        Me.btnAddRow.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnAddRow.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnAddRow.Location = New System.Drawing.Point(545, 379)
+        Me.btnAddRow.Name = "btnAddRow"
+        Me.btnAddRow.Size = New System.Drawing.Size(102, 32)
+        Me.btnAddRow.TabIndex = 1
+        Me.btnAddRow.Text = "Add Row"
+        Me.btnAddRow.UseVisualStyleBackColor = True
+        '
+        'btnRemoveRow
+        '
+        Me.btnRemoveRow.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnRemoveRow.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnRemoveRow.Location = New System.Drawing.Point(653, 379)
+        Me.btnRemoveRow.Name = "btnRemoveRow"
+        Me.btnRemoveRow.Size = New System.Drawing.Size(102, 32)
+        Me.btnRemoveRow.TabIndex = 2
+        Me.btnRemoveRow.Text = "Remove Row"
+        Me.btnRemoveRow.UseVisualStyleBackColor = True
+        '
+        'frmPostcodeDataGrid
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(767, 420)
+        Me.Controls.Add(Me.btnRemoveRow)
+        Me.Controls.Add(Me.btnAddRow)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Font = New System.Drawing.Font("Tahoma", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.KeyPreview = True
+        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Name = "frmPostcodeDataGrid"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Postcode Thailand in DataGrid - coDe bY: Thongkorn Tubtimkrob"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents btnAddRow As System.Windows.Forms.Button
+    Friend WithEvents btnRemoveRow As System.Windows.Forms.Button
+
+End Class
---
+++ PostcodeDataGrid/PostcodeDataGrid/frmPostcodeDataGrid.vb
@@ -0,0 +1,229 @@
+﻿#Region "ABOUT"
+' / --------------------------------------------------------------------
+' / Developer : Mr.Surapon Yodsanga (Thongkorn Tubtimkrob)
+' / eMail : thongkorn@hotmail.com
+' / URL: http://www.g2gnet.com (Khon Kaen - Thailand)
+' / Facebook: https://www.facebook.com/g2gnet (For Thailand)
+' / Facebook: https://www.facebook.com/commonindy (Worldwide)
+' / More Info: http://www.g2gsoft.com/
+' /
+' / Purpose: Postcode Thailand on DataGridView
+' / Microsoft Visual Basic .NET (2010) + MS Access 2007
+' /
+' / This is open source code under @Copyleft by Thongkorn Tubtimkrob.
+' / You can modify and/or distribute without to inform the developer.
+' / --------------------------------------------------------------------
+#End Region
+
+Imports System.Data.OleDb
+
+Public Class frmPostcodeDataGrid
+    Dim Conn As New OleDbConnection
+    Dim Cmd As New OleDbCommand
+    Dim DR As OleDbDataReader
+    Dim DA As New OleDbDataAdapter
+    Dim Sql As String = String.Empty
+    Dim ColProvince As New DataGridViewComboBoxColumn
+    '//
+    Private Sub frmPostcodeDataGrid_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        Dim ColAmphur As New DataGridViewComboBoxColumn
+        Dim ColTumbon As New DataGridViewComboBoxColumn
+        Dim ColPostCode As New DataGridViewTextBoxColumn
+        '// กำหนด Path ของไฟล์ฐานข้อมูล
+        Dim strPath As String = Application.StartupPath
+        strPath = strPath.ToLower()
+        strPath = strPath.Replace("\bin\debug", "\").Replace("\bin\release", "\")
+
+        Dim strConn As String = _
+            " Provider=Microsoft.ACE.OLEDB.12.0;" & _
+            " Data Source = " & strPath & "Data\PostCode2555.accdb; " & _
+            " Persist Security Info=False;"
+        Try
+            '// เปิดการเชื่อมต่อไฟล์ฐานข้อมูล
+            Conn = New OleDb.OleDbConnection(strConn)
+            Conn.Open()
+            With DataGridView1
+                .AllowUserToAddRows = False
+                .AllowUserToDeleteRows = False
+                .SelectionMode = DataGridViewSelectionMode.CellSelect
+                .AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill
+                '.AutoResizeColumns()
+                .MultiSelect = False
+            End With
+            With ColProvince
+                .Name = "Province"
+                .HeaderText = "จังหวัด"
+                .ReadOnly = False
+                .MaxDropDownItems = 10
+            End With
+            DataGridView1.Columns.Add(ColProvince)
+            Call LoadProvice()
+            '//
+            With ColAmphur
+                .Name = "Amphur"
+                .HeaderText = "อำเภอ/เขต"
+                .ReadOnly = False
+                .MaxDropDownItems = 10
+            End With
+            DataGridView1.Columns.Add(ColAmphur)
+            '//
+            With ColTumbon
+                .Name = "Tumbon"
+                .HeaderText = "ตำบล"
+                .ReadOnly = False
+                .MaxDropDownItems = 10
+            End With
+            DataGridView1.Columns.Add(ColTumbon)
+            '//
+            With ColPostCode
+                .ReadOnly = True
+                .HeaderText = "รหัสไปรษณีย์"
+                .Name = "PostCode"
+            End With
+            DataGridView1.Columns.Add(ColPostCode)
+            '// Add new row.
+            DataGridView1.Rows.Add("", "", "", "")
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+            Application.Exit()
+        End Try
+
+    End Sub
+
+    ' / โหลดรายชื่อจังหวัดเข้าสู่ ComboBox
+    Sub LoadProvice()
+        '// DISTINCT คือ หากชื่อรายการมันซ้ำ ต้องตัดให้เหลือเพียงรายการเดียว
+        Sql = _
+            " SELECT DISTINCT PostCode.Province " & _
+            " From PostCode ORDER BY PostCode.Province "
+        Cmd = New OleDbCommand(Sql, Conn)
+        DR = Cmd.ExecuteReader
+        While DR.Read()
+            ColProvince.Items.Add(DR("Province").ToString)
+        End While
+        DR.Close()
+        Cmd.Dispose()
+    End Sub
+
+    Sub LoadAmphur(ByVal Province As String, ByVal row As Integer)
+        Sql = _
+                " SELECT DISTINCT PostCode.Amphur, PostCode.Province " & _
+                " From PostCode  " & _
+                " WHERE " & _
+                " Province = " & "'" & Province & "'" & _
+                " ORDER BY PostCode.Amphur "
+        Try
+            Dim Grow As DataGridViewRow = DataGridView1.Rows(row)
+            Dim MyCell As DataGridViewComboBoxCell
+            MyCell = Grow.Cells.Item("Amphur")
+            If Conn.State = ConnectionState.Closed Then Conn.Open()
+            DA = New OleDb.OleDbDataAdapter(Sql, Conn)
+            Dim DT As New DataTable
+            DA.Fill(DT)
+            MyCell.Items.Clear()
+            For Each DR As DataRow In DT.Rows
+                MyCell.Items.Add(DR("Amphur"))
+            Next
+            DT.Dispose()
+            DA.Dispose()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+
+    Sub LoadTumbon(ByVal Province As String, ByVal Amphur As String, ByVal row As Integer)
+        Sql = _
+            " SELECT PostCode.Amphur, PostCode.Province, PostCode.Tumbon " & _
+            " From PostCode  " & _
+            " WHERE " & _
+            " Province = " & "'" & Province & "'" & _
+            " AND " & _
+            " Amphur = " & "'" & Amphur & "'" & _
+            " ORDER BY PostCode.Tumbon "
+        Try
+            Dim Grow As DataGridViewRow = DataGridView1.Rows(row)
+            Dim MyCell As DataGridViewComboBoxCell
+            MyCell = Grow.Cells.Item("Tumbon")
+            If Conn.State = ConnectionState.Closed Then Conn.Open()
+            DA = New OleDb.OleDbDataAdapter(Sql, Conn)
+            Dim DT As New DataTable
+            DA.Fill(DT)
+            MyCell.Items.Clear()
+            For Each DR As DataRow In DT.Rows
+                MyCell.Items.Add(DR("Tumbon"))
+            Next
+            DT.Dispose()
+            DA.Dispose()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+
+    Sub LoadPostCode(ByVal Province As String, ByVal Amphur As String, ByVal Tumbon As String, ByVal row As Integer)
+        Sql = _
+            " SELECT PostCode.Province, PostCode.Amphur, PostCode.Tumbon, " & _
+            " PostCode.PostCode, PostCode.Remark " & _
+            " From PostCode  " & _
+            " WHERE " & _
+            " Province = " & "'" & Province & "'" & _
+            " AND " & _
+            " Amphur = " & "'" & Amphur & "'" & _
+            " AND " & _
+            " Tumbon = " & "'" & Tumbon & "'" & _
+            " ORDER BY PostCode.Tumbon "
+        Try
+            If Conn.State = ConnectionState.Closed Then Conn.Open()
+            Cmd = New OleDbCommand(Sql, Conn)
+            DR = Cmd.ExecuteReader
+            While DR.Read()
+                DataGridView1.Rows(row).Cells(3).Value = DR.Item("PostCode").ToString
+            End While
+            DR.Close()
+            Cmd.Dispose()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+
+    ' / --------------------------------------------------------------------------------
+    ' / Remove selected row
+    Private Sub btnRemoveRow_Click(sender As System.Object, e As System.EventArgs) Handles btnRemoveRow.Click
+        If DataGridView1.RowCount = 0 Then Exit Sub
+        DataGridView1.Rows.Remove(DataGridView1.CurrentRow)
+    End Sub
+
+    Private Sub btnAddRow_Click(sender As System.Object, e As System.EventArgs) Handles btnAddRow.Click
+        DataGridView1.Rows.Add("", "", "", "")
+    End Sub
+
+    Private Sub DataGridView1_CellEndEdit(sender As Object, e As System.Windows.Forms.DataGridViewCellEventArgs) Handles DataGridView1.CellEndEdit
+        Dim Province As String = DataGridView1.Rows(e.RowIndex).Cells(0).Value
+        Dim Amphur As String = DataGridView1.Rows(e.RowIndex).Cells(1).Value
+        Dim Tumbon As String = DataGridView1.Rows(e.RowIndex).Cells(2).Value
+        Select Case e.ColumnIndex
+            Case 0
+                DataGridView1(1, e.RowIndex).Value = Nothing
+                '// ส่งค่าจังหวัดและแถวปัจจุบัน
+                Call LoadAmphur(Province, e.RowIndex)
+            Case 1
+                DataGridView1(2, e.RowIndex).Value = Nothing
+                '// ส่งค่าจังหวัด อำเภอและแถวปัจจุบัน
+                If Province <> Nothing And Amphur <> Nothing Then Call LoadTumbon(Province, Amphur, e.RowIndex)
+            Case 2
+                DataGridView1(3, e.RowIndex).Value = Nothing
+                '// ส่งค่าจังหวัด อำเภอ ตำบลและแถวปัจจุบัน
+                If Province <> Nothing AndAlso Amphur <> Nothing AndAlso Tumbon <> Nothing Then
+                    Call LoadPostCode(Province, Amphur, Tumbon, e.RowIndex)
+                End If
+        End Select
+    End Sub
+
+    Private Sub DataGridView1_CellMouseLeave(sender As Object, e As System.Windows.Forms.DataGridViewCellEventArgs) Handles DataGridView1.CellMouseLeave
+        Select Case e.ColumnIndex
+            '// Update
+            Case 2
+                DataGridView1.EndEdit()
+        End Select
+    End Sub
+End Class
